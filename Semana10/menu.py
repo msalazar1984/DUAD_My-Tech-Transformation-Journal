7caf7dc2b7cd3a752c29c1956fcb6e447dc6282e
msalazar1984 <miguelsarojas@gmail.com>
2025-04-09 21:30:39 -0600

    Semana10V3

--- Semana10/menu.py
+++ Semana10/menu.py
@@ -3,7 +3,6 @@ import actions,data
 
 def menu(my_saved_list):
     num_option=0
-    my_boolean=False
     system_on=True
     
     while system_on==True:
@@ -12,24 +11,23 @@ def menu(my_saved_list):
             if num_option==1:
                 actions.input_information(my_saved_list)
             elif num_option==2:
-                print(actions.open_csvfile(my_saved_list))
+                print(my_saved_list)
             elif num_option==3:
                 print(actions.sorting_top3students(my_saved_list))
             elif num_option==4:
                 actions.calculate_total_average(my_saved_list)
             elif num_option==5:
                 data.export_file(my_saved_list)
-                if my_saved_list!=[]:
-                    my_boolean=True
-                else:
-                    my_boolean=False
             elif num_option==6:
-                data.import_file(my_boolean,my_saved_list)
-                my_boolean=False
+                print(data.import_file(my_saved_list))
+                #print(my_saved_list)
             elif num_option==7:
                 system_on=False
+            elif num_option==2:
+                print(actions.open_csvfile(my_saved_list))
             if num_option>7 or num_option<1:
                 raise Exception
+            
         except ValueError:
             print("Por favor Ingrese un número en el Menú no se permiten letras o signos")
         except Exception as error:
